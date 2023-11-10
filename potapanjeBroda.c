c8703eef2af0aadb9d583c0d2f8c69f0abe4808e
aawad1 <aawad1@etf.unsa.ba>
2023-11-10 19:53:42 +0100

    dodao 3 funkcije

--- potapanjeBroda.c
+++ potapanjeBroda.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-int main (void){
 
-    printf("Dobro došli u igricu potapanje brodova.\nU ovoj igrici igraju dva igrača.\n");
-    printf("Igrač broj 1: unesite podatke broda u formatu (x,y,z,w), gdje su x=red broda, y=kolona broda, z=duzina broda, w=orijentacija broda gdje je n-north, s-south, e-east i w-west.\n");
-    printf("Pazite da duzina broda ne smije prekoračiti van table, u suprotnom se traži ponovni unos.\n");
-    printf("Unos: ");
-    int row, colon, size;
-    char orientation;
-    char inputString[20];
-    scanf("%s ", inputString);
-    sscanf(inputString, "%d,%d,%c,%d", &row, &colon, &orientation, &size);
-    
-    int polje1[10][10] = {{0}}; 
-    for (int j=0; j<10; j++){
-            printf(" _");
+void unesi(char niz[], int velicina) {
+    char znak = getchar();
+    if (znak == '\n') znak = getchar();
+    int i = 0;
+    while (i < velicina-1 && znak != '\n') {
+        niz[i] = znak;
+        i++;
+        znak = getchar();
     }
-    printf("\n");
-    for (int i=0; i<10; i++){       
+    niz[i] = '\0';
+ }
+
+void iscrtajPolje(){
+
+    printf("  0 1 2 3 4 5 6 7 8 9\n");
+    printf("  _ _ _ _ _ _ _ _ _ _\n");
+
+    for (int i=0; i<10; i++){     
+        printf("%d", i);  
         for (int j=0; j<10; j++){
             printf("|_");
         }
         printf("|\n");
     }
+}
+
+void opisIgre(){
+    printf("Dobro došli u igricu potapanje brodova.\nU ovoj igrici igraju dva igraca.\n");
+    printf("Igrač broj 1: unesite podatke za 3 broda velicina 5, 4 i 3 u formatu (x,y,z), gdje su x=red broda, y=kolona broda, z=orijentacija broda gdje je n-north, s-south, e-east i w-west.\n");
+    printf("Obavezni uslovi: | x>=0 i x<10 | - | y>=0 i y<10 | - | z = n/s/e/w |");
+    printf("Pazite da duzina broda ne smije prekoraciti van table, u suprotnom se trazi ponovni unos.\n");
+}
+
+int main (void){
+
+    opisIgre();
+    char row, colon, orientation;
+    char inputString[20];
     
+    
+    //unosi podatke i dodijeli ih parametrima red, kolona, duzina broda i orijentacija
+    iscrtajPolje();
+    unesi(inputString, strlen(inputString));
+    sscanf(inputString, "%c,%c,%c", &row, &colon, &orientation);
+    
+    int polje1[10][10] = {{0}};   
 
     return 0;
 }
